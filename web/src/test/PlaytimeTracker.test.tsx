e989c37266a240cf34564c5e6f643faf9a553ae0
Michael duBois <mdubois@bytement.com>
2026-03-23 21:08:36 -0600

    Fix Failing Tests

--- web/src/test/PlaytimeTracker.test.tsx
+++ web/src/test/PlaytimeTracker.test.tsx
@@ -1,32 +1,55 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import Home from "../Home";
-import { expect, test, describe } from "vitest";
+import { beforeEach, expect, test, describe } from "vitest";
 
 describe("Home Component", () => {
-  test("renders the app title", () => {
+  beforeEach(() => {
+    localStorage.clear();
     render(<Home />);
+  })
+
+  test("renders the app title", () => {
     expect(screen.getByText("Leagueify Playtime Tracker")).toBeInTheDocument();
   });
 
   test("adds a player when clicking 'Add Player' button", () => {
-    render(<Home />);
     const addButton = screen.getByText("Add Player");
     fireEvent.click(addButton);
+    expect(screen.getByDisplayValue("Player 1")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 2")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 3")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 4")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 5")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 6")).toBeInTheDocument();
     expect(screen.getByDisplayValue("Player 7")).toBeInTheDocument();
   });
 
   test("toggles player status between 'Bench' and 'In Play'", () => {
-    render(<Home />);
     const toggleButton = screen.getAllByText("Bench")[0];
     fireEvent.click(toggleButton);
     expect(toggleButton).toHaveTextContent("In Play");
   });
 
   test("resets all timers when clicking 'Reset Game'", () => {
-    render(<Home />);
     fireEvent.click(screen.getByText("Reset Game"));
     fireEvent.click(screen.getByText("Yes, Reset"));
     const timers = screen.getAllByText("00:00");
-    expect(timers.length).toBeGreaterThanOrEqual(1);
+    expect(timers.length).toBe(12);
   });
+
+  test("resets application when clicking 'Reset App'", () => {
+    const addButton = screen.getByText("Add Player");
+    fireEvent.click(addButton);
+    expect(screen.getByDisplayValue("Player 1")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 2")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 3")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 4")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 5")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 6")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("Player 7")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("Reset App"));
+    fireEvent.click(screen.getByText("Yes, Reset"));
+    const timers = screen.getAllByText("00:00");
+    expect(timers.length).toBe(12);
+  })
 });
